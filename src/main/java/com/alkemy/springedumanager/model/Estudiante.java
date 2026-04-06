a21fbd47a8ae8392951159f8dcce2a75ee015cea
carlos <casepulveda456@gmail.com>
2026-04-06 10:50:08 -0400

    commit: fix estudiante.java

--- src/main/java/com/alkemy/springedumanager/model/Estudiante.java
+++ src/main/java/com/alkemy/springedumanager/model/Estudiante.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
@@ -16,6 +17,7 @@ import java.util.Set;
 @Table(name = "estudiantes")
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(exclude = "usuario")
 public class Estudiante {
 
     @Id
